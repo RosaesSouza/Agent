1f4bb84df5f1c195cf7af6bcccb42713ed616a27
RosaesSouza <gabrielrosaes@icloud.com>
2025-12-11 11:37:51 -0300

    Capitulo 1

---
+++ ch01/hello_llm.py
@@ -0,0 +1,28 @@
+# hello_llm.py
+import os
+from dotenv import load_dotenv
+#from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI 
+#from langchain_anthropic import ChatAnthropic
+
+from langchain_core.messages import HumanMessage
+
+
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Criar instância do modelo
+modelo = ChatGoogleGenerativeAI(
+    model=os.getenv("GOOGLE-MODEL", "gemini-2.5-flash-lite"),
+    temperature=0  # 0 = determinístico, 1 = criativo
+)
+
+# Criar uma mensagem do usuário
+mensagem = HumanMessage(content="Olá! Qual é a capital do Brasil?")
+
+# Invocar o modelo
+resposta = modelo.invoke([mensagem])
+
+# Exibir a resposta
+print(f"Resposta do modelo: {resposta.content}")
